4df4e0841d39a57a2089c0d645b9cb0fe32b6ace
DelfinaMolter <delfi.molter@gmail.com>
2023-09-08 09:55:31 -0300

    cambio en faqs

--- pages/preguntas-frecuentes/index.page.tsx
+++ pages/preguntas-frecuentes/index.page.tsx
@@ -11,7 +11,7 @@ const Index: NextPage = () => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <BodySingle title={"FAQs"}>
+            <BodySingle title={"FAQs page"}>
             </BodySingle>
         </>
     )
