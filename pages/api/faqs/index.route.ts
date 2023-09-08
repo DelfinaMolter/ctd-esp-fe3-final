ba2457436d04a1cc3fb41d3b3c2e498bcf471564
DelfinaMolter <delfi.molter@gmail.com>
2023-09-08 20:46:35 -0300

    variable de entorno BASE_URL

--- pages/api/faqs/index.route.ts
+++ pages/api/faqs/index.route.ts
@@ -6,10 +6,10 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
     if (req.method === "GET") {
-        // const faq = faqsData.length
-        // if(faq === 0) {
-        //     return res.status(404).json({ message: `Proximamente` })
-        // }
+        const faq = faqsData.length
+        if(faq === 0) {
+            return res.status(404).json({ message: `Proximamente` })
+        }
         return res.status(200).json(faqsData)
     } else {
         res.status(400).json({ message: "Método no permitido" })
--- pages/preguntas-frecuentes/index.page.tsx
+++ pages/preguntas-frecuentes/index.page.tsx
@@ -3,11 +3,11 @@ import Head from 'next/head'
 import BodySingle from "dh-marvel/components/layouts/body/single/body-single";
 import { Faq } from 'interface/faqs';
 
-// interface Props{
-//     faqs: Faq[]
-// }
-const faqsPage: NextPage = () => {
-// const faqsPage: NextPage<Props> = ({ faqs }) => {
+interface Props{
+    faqs: Faq[]
+}
+
+const faqsPage: NextPage<Props> = ({ faqs }) => {
     return (
         <>
             <Head>
@@ -17,31 +17,30 @@ const faqsPage: NextPage = () => {
             </Head>
 
             <BodySingle title={"FAQs page"}>
-            {/* <div >
+            <div >
                 {faqs.map((faq) => (
                     <div key={faq.id}>
                         <h2>{faq.question}</h2>
                         <p>{faq.answer}</p>
                     </div>
                     ))}
-            </div> */}
+            </div>
             </BodySingle>
         </>
     )
 }
 
-// export const getStaticProps = async () => {
+export const getStaticProps = async () => {
 
-//     // Modificar la url por las que nos da Vercel al hacer deploy
-//     const response = await fetch('http://localhost:3000/api/faqs')
-//     const faqs = await response.json()
+    const response = await fetch(`${process.env.BASE_URL}/api/faqs`)
+    const faqs = await response.json()
 
-//     return {
-//         props: {
-//             faqs
-//         }
-//     }
+    return {
+        props: {
+            faqs
+        }
+    }
 
-// }
+}
 
 export default faqsPage
